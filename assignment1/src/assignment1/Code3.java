d261efde90608012fe51cb327e0ce932556781f7
supriyaa patnaik <supriyaa patnaik@LAPTOP-60P4L3HN>
2022-05-22 11:59:23 +0530

    third commit

---
+++ assignment1/src/assignment1/Code3.java
@@ -0,0 +1,13 @@
+package assignment1;
+
+public class Code3
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		String var = new String("Hello Capgemini");
+		System.out.println(var);
+	}
+
+}
