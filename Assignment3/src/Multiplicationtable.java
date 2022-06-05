d9dc2932d108e0ec6d5093c04e436981f739309b
supriyaa patnaik <supriyaa patnaik@LAPTOP-60P4L3HN>
2022-06-05 21:53:22 +0530

    first commit

---
+++ Assignment3/src/Multiplicationtable.java
@@ -0,0 +1,20 @@
+
+public class Multiplicationtable 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		
+		{
+			int num=4;
+			for(int i=1;i<=10;++i)
+			{
+				//System.out.println(num);
+				System.out.printf("%d*%d=%d \n",num,i,num*i);
+			}
+		}
+
+	}
+
+}
